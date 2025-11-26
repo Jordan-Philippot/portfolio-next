1d4748756dfb5785e5f2884bbec4cdf612c7b88a
Jordan P <jordan.philippot@epitech.eu>
2025-11-26 01:43:08 +0100

    Contact + Legal Notice pages

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,1 +1,16 @@
+'use client'
 
+import {useRef} from 'react'
+
+// ----- Components -----
+import ContactMeComponent from '@/components/home/contactMe/ContactMe'
+
+
+export default function Contact() {
+    const ContactMeComponentRef = useRef<HTMLDivElement>(null)
+
+
+    return (
+        <ContactMeComponent ref={ContactMeComponentRef}/>
+    )
+}
---
+++ app/mentions-legales/page.tsx
@@ -0,0 +1,7 @@
+'use client'
+
+export default function LegalNotice() {
+    return (
+        <>Mentions Légales</>
+    )
+}
