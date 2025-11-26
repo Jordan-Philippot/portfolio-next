067ec61e843c20e519edd0cc8a93adf32eb98d4d
Jordan P <jordan.philippot@epitech.eu>
2025-11-26 01:45:47 +0100

    Fix ref projects ref

--- app/projets/page.tsx
+++ app/projets/page.tsx
@@ -1,14 +1,16 @@
 'use client'
 import ProjectsComponent from '../../components/home/myProjects/Projects'
+import {useRef} from "react";
 
 
 export default function Projects() {
+    const ProjectsComponentRef = useRef<HTMLDivElement>(null)
 
 
     return (
         <div id="projects" className="page">
             {/* ----- MY PROJECTS ----- */}
-            <ProjectsComponent/>
+            <ProjectsComponent ref={ProjectsComponentRef}/>
         </div>
     )
 }
